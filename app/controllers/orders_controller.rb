855531be06ee12be864fb12da9c5d0e50a59c5c7
Miguel Tavarez <tavarezster@gmail.com>
2017-06-16 17:34:23 +0000

    collection partials including collections nested within collection object

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,13 @@
+# require 'pry'
+
 class OrdersController < ApplicationController
 
   def index
+    # binding.pry
     @orders = Order.all
+    # @products = @orders.collect {|order| OrderedProduct.find_by(order_id: order.id).order.products}
+    # binding.pry
+    # @ordered_products = [@orders, @products]
   end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,5 @@ class Order < ActiveRecord::Base
   has_many :ordered_products
   has_many :products, :through => :ordered_products
   belongs_to :customer
+  
 end
